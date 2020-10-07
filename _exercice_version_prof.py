8ac911923242b969f5f62304b796e4ded1fd945c
kegaub <kevin.gauthier.22.06@gmail.com>
2020-10-06 20:01:21 -0400

    update exercice version prof partie 1

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def capitaliser_pays(nom):
-    # Ceci est la version pour le/la professeur(e)
-    # TODO completer la fonction
-    return nom
+import math
+import sys
+sys.path.insert(1, 'PUT_YOUR_PATHHERE')
+from c04_ch6_exercices.exercice import frequence
+
+
+
+def compute_volume_and_mass(a=2, b=4, c=6, masse_vol=10):
+    volume = math.pi * a * b * c * 4 / 3
+    masse = volume * masse_vol
+
+    return volume, masse
 
 
 if __name__ == '__main__':
-    pays = [
-        'AfghanIstan',
-        'albania',
-        'algeria',
-        'AndorRa',
-        'angolA',
-        'antigua ANd barbuda',
-        'argEntina',
-        'Armenia',
-        'austrAlia',
-        'ausTria',
-        'azerBaijaN'
-    ]
-    for i in range(len(pays)):
-        pays[i] = capitaliser_pays(pays[i])
+    # TODO: Appelez vos fonctions ici
+    print(compute_volume_and_mass())
+    print((lambda sentence: sorted(frequence(sentence), key=frequence(sentence).__getitem__)[-1])("bbbig bbbig test"))
 
-    print(pays)
+    pass
